2c59720eec5700c0c3e9dba442ef2d2d62139c4f
dyanaamer <diyanaamer@gmail.com>
2019-08-06 22:02:01 +0800

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,9 +85,9 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://laravel.com/docs">Doc</a>
+                    <a href="https://laracasts.com">Laracast</a>
+                    <a href="https://laravel-news.com">New</a>
                     <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
